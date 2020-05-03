e09de810fd8b43e0f67b1ce845c54be55de344d4
Giancarlo Colmenares <giancarlo.cs@gmail.com>
2020-05-03 18:44:46 -0400

    Updated multi agent class, changed buffer and noise classes

--- ddpg.py
+++ ddpg.py
@@ -11,40 +11,46 @@ import numpy as np
 # add OU noise for exploration
 from OUNoise import OUNoise
 
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = 'cpu'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = 'cpu'
 
 class DDPGAgent:
-    def __init__(self, in_actor, out_actor, in_critic, lr_actor=1.0e-2, lr_critic=1.0e-2):
+    def __init__(self, in_actor, out_actor, in_critic, lr_actor=1.0e-4, lr_critic=1.0e-3):
         super(DDPGAgent, self).__init__()
 
-        hidden_in_actor = 32
-        hidden_out_actor = 32
+        hidden_in_actor = 64
+        hidden_out_actor = 128
         hidden_in_critic = hidden_in_actor
         hidden_out_critic = hidden_out_actor
-        
+
         self.actor = Network(in_actor, hidden_in_actor, hidden_out_actor, out_actor, actor=True).to(device)
-        self.critic = Network(in_critic, hidden_in_critic, hidden_out_critic, 1).to(device)
+        self.critic = Network(in_critic, hidden_in_critic, hidden_out_critic, 1, out_actor, actor=False).to(device)
         self.target_actor = Network(in_actor, hidden_in_actor, hidden_out_actor, out_actor, actor=True).to(device)
-        self.target_critic = Network(in_critic, hidden_in_critic, hidden_out_critic, 1).to(device)
+        self.target_critic = Network(in_critic, hidden_in_critic, hidden_out_critic, 1, out_actor, actor=False).to(device)
 
-        self.noise = OUNoise(out_actor, scale=1.0 )
-
+        self.noise = OUNoise(out_actor, scale=0.9) #scale 1.0
+        self.noise_shape = out_actor
         
         # initialize targets same as original networks
         hard_update(self.target_actor, self.actor)
         hard_update(self.target_critic, self.critic)
 
         self.actor_optimizer = Adam(self.actor.parameters(), lr=lr_actor)
-        self.critic_optimizer = Adam(self.critic.parameters(), lr=lr_critic, weight_decay=1.e-5)
+        WD = 1e-5
+        self.critic_optimizer = Adam(self.critic.parameters(), lr=lr_critic, weight_decay=WD)
 
+    def reset(self):
+        self.noise.reset()
+    
+    def noisef(self, mean=0, sigma=0.08) :
+        return np.random.normal(mean, sigma, self.noise_shape)
 
     def act(self, obs, noise=0.0):
         obs = obs.to(device)
-        action = self.actor(obs) + noise*self.noise.noise()
+        action = self.actor(obs).cpu().data.numpy() + noise * self.noisef() #self.noise.noise()
         return action
 
     def target_act(self, obs, noise=0.0):
         obs = obs.to(device)
-        action = self.target_actor(obs) + noise*self.noise.noise()
+        action = self.target_actor(obs).cpu() # + noise * self.noisef() #self.noise.noise()
         return action
--- networkforall.py
+++ networkforall.py
@@ -9,17 +9,32 @@ def hidden_init(layer):
     return (-lim, lim)
 
 class Network(nn.Module):
-    def __init__(self, input_dim, hidden_in_dim, hidden_out_dim, output_dim, actor=False):
+    def __init__(self, input_dim, hidden_in_dim, hidden_out_dim, output_dim, act_size = 1, actor=False, seed=2):
         super(Network, self).__init__()
 
         """self.input_norm = nn.BatchNorm1d(input_dim)
         self.input_norm.weight.data.fill_(1)
         self.input_norm.bias.data.fill_(0)"""
-        self.fc1 = nn.Linear(input_dim,hidden_in_dim)
-        self.fc2 = nn.Linear(hidden_in_dim,hidden_out_dim)
+        
+        self.seed = torch.manual_seed(seed)
+        
+        if actor :
+            self.fc1 = nn.Linear(input_dim, hidden_in_dim)
+            self.fc2 = nn.Linear(hidden_in_dim, hidden_out_dim)
+        else :
+            self.fc1 = nn.Linear(input_dim, hidden_in_dim)
+            self.fc2 = nn.Linear(hidden_in_dim + act_size * 2, hidden_out_dim)
+        
         self.fc3 = nn.Linear(hidden_out_dim,output_dim)
-        self.nonlin = f.relu #leaky_relu
+        #self.fc4 = nn.Linear(256, output_dim)
+        self.nonlin = nn.LeakyReLU(0.01) #leaky_relu
         self.tanh = torch.tanh
+        
+        self.bn_input = nn.BatchNorm1d(hidden_in_dim)
+        self.bn_hid = nn.BatchNorm1d(output_dim)
+
+        #Dropout
+        self.dpout = nn.Dropout(p=0.30)
         
         self.actor = actor
         self.reset_parameters()
@@ -27,22 +42,35 @@ class Network(nn.Module):
     def reset_parameters(self):
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-1e-3, 1e-3)
+        #self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
-    def forward(self, x):
+    def forward(self, x, act=None):
         if self.actor:
             # return a vector of the force
-            h1 = self.nonlin(self.fc1(x))
+            x = self.fc1(x)
+            x = self.bn_input(x)
+            x = self.nonlin(x)
+#             bn2 = self.bn_hid(self.fc2(h1))
+            x = self.nonlin(self.fc2(x))
+            x = self.fc3(x)
+            x = self.bn_hid(x)
+            #x = self.fc4(x)
 
-            h2 = self.nonlin(self.fc2(h1))
-            h3 = (self.fc3(h2))
-            
-            # h3 is a 2D vector (a force that is applied to the agent)
-            return self.tanh(h3)
-        
+            return self.tanh(x)
         else:
             # critic network simply outputs a number
-            h1 = self.nonlin(self.fc1(x))
-            h2 = self.nonlin(self.fc2(h1))
-            h3 = (self.fc3(h2))
-            return h3
+            #bn = self.bn_input(self.fc1(x))
+            x = self.fc1(x)
+            x = self.bn_input(x)
+            x = self.nonlin(x)
+            x = self.dpout(x)
+            x = torch.cat((x, act), dim=1)
+#             bn2 = self.bn_hid(self.fc2(h1))
+            x = self.nonlin(self.fc2(x))
+            x = self.dpout(x)
+            x = self.fc3(x)
+            #x = self.dpout(x)
+            #x = self.fc4(x)
+
+            return x
